24bd9b7225936771210a36b6ec428c3bad834cd0
Hassan Allam <hassan309pro@gmail.com>
2025-08-12 18:29:29 +0300

    LINQ - Restriction Operators 1. Find all products that are out of stock.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LNIQ_Assignment_01
+﻿using LNIQ_Assignment_01;
+using static LNIQ_Assignment_01.ListGenerator;
+
+namespace LINQ_Assignment_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region LINQ - Restriction Operators
+            #region 1. Find all products that are out of stock.
+            //var Result = ProductList.Where(P => P.UnitsInStock == 0);
+            //
+            //foreach(var product in Result)
+            //{
+            //    Console.WriteLine(product);
+            //}
+            #endregion
+            #endregion
         }
     }
 }
